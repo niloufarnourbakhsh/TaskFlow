a7ede433a0912b32369a1dcd8860d279669e8a21
niloofar nourbakhsh <niloofar.noorbakhsh@yahoo.com>
2024-07-02 01:05:20 +0330

    composer view is written for categories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,16 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\Plan;
+use App\Policies\PlanPolicy;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Register any application services.
      */
@@ -19,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer(['Plan.create'],function ($view){
+            $view->with('Categories',Category::all());
+        });
+        Gate::policy(Plan::class,PlanPolicy::class);
     }
 }
